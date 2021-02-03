b83a58fa553cc96cf7d62a80f9579d9aa314e802
jzamora5 <jzamora_5@yahoo.com>
2021-02-03 12:51:23 -0500

    Push feature branch

--- aic_frontend/src/containers/Register.jsx
+++ aic_frontend/src/containers/Register.jsx
@@ -2,7 +2,47 @@ import React from "react";
 import "../assets/styles/components/Register.scss";
 
 const Register = () => {
-  return <h1>Register</h1>;
+  return (
+    <div className="container py-5">
+      <div className="card">
+        <h2>Register</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
